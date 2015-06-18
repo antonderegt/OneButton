6aae07ee64089db2d2139ad64bba8efbdb710d72
antonderegt <antonderegt@gmail.com>
2015-06-18 22:07:57 +0200

    Added weirdswitch

--- oneButton.ino
+++ oneButton.ino
@@ -2,8 +2,16 @@
 #include <NewRemoteTransmitter.h>
 
 const int numberButtons = 4;
+const int stateOnePin = 6;
+const int stateTwoPin = 4;
+const int turnOff = 0;
 
-int buttonPin[numberButtons] = {9,5,3,7};
+int stateOne = 0;
+int stateTwo = 0;
+int stateThree = 0;
+int whatToSwitch = 0;
+
+int buttonPin[numberButtons] = {9,3,7,5};
 int buttonState[numberButtons];
 int lastButtonState[numberButtons];
 int lastDebounceTime[numberButtons];
@@ -20,6 +28,9 @@ NewRemoteTransmitter transmitter(15986790, 12, 260, 3);
 
 void setup() {
   buttonSetup();
+  pinMode(stateOnePin, INPUT);
+  pinMode(stateTwoPin, INPUT);
+  Serial.begin(9600);
   
     // Initialise the IO and ISR
   vw_set_ptt_inverted(true);  // Required for DR3100
@@ -27,6 +38,26 @@ void setup() {
 }
 
 void loop() {
+  whatToSwitch = checkWeirdSwitch();
+  switch(whatToSwitch){
+    case 1:
+    Serial.println("One");
+      buttonHandler();
+      break;
+    case 2:
+    Serial.println("Two");
+      ledHandler();
+      break;
+    case 3:
+    Serial.println("Three");
+      turnEverythingOff();
+      break;
+    default:
+      buttonHandler();
+  }
+}
+
+void buttonHandler(){
   // read the state of the switch into a local variable:
   for (int x = 0; x < numberButtons; x++){
     int reading = digitalRead(buttonPin[x]);
@@ -50,6 +81,62 @@ void loop() {
     
     lastButtonState[x] = reading;
   }
+}
+
+void ledHandler(){
+  // turn LED strip on or off.
+}
+
+void turnEverythingOff(){
+  // read the state of the switch into a local variable:
+  for (int x = 0; x < numberButtons; x++){
+    int reading = digitalRead(buttonPin[x]);
+  
+    if (reading != lastButtonState[x]) {
+      // reset the debouncing timer
+      lastDebounceTime[x] = millis();
+    } 
+    
+    if ((millis() - lastDebounceTime[x]) > debounceDelay) {
+      if (reading != buttonState[x]) {
+        buttonState[x] = reading;
+  
+        // only toggle the LED if the new button state is HIGH
+        if (buttonState[x] == HIGH) {
+          for (int j = 0; j < numberButtons; j++){
+            transmitter.sendUnit(j, turnOff);
+            // delay ?
+          }
+          // @TODO Turn LED strip off
+        }
+      }
+    }
+    
+    lastButtonState[x] = reading;
+  }
+}
+
+int checkWeirdSwitch(){
+  if(digitalRead(stateOnePin) && !stateOne){
+    stateOne = 1;
+    stateTwo = 0;
+    stateThree = 0;
+    Serial.println("StateOne");
+    return 1;
+  } else if(digitalRead(stateTwoPin) && !stateTwo){
+    stateOne = 0;
+    stateTwo = 1;
+    stateThree = 0;
+    Serial.println("StateTwo");
+    return 2;
+  } else if(!digitalRead(stateOnePin) && !digitalRead(stateTwoPin) && !stateThree){
+    stateOne = 0;
+    stateTwo = 0;
+    stateThree = 1;
+    Serial.println("StateThree");
+    return 3;
+  }
+  Serial.println("Stop");
 }
 
 void buttonSetup()
